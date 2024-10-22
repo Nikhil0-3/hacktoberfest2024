e50d251abb95765cdf5403ea0cf735f899fd4cf8
Nikhil More <149102391+Nikhil0-3@users.noreply.github.com>
2024-10-22 15:14:33 +0530

    Update avg.py

--- avg.py
+++ avg.py
@@ -1,6 +1,7 @@
+
 a=input ("enter first number")
 b=input ("enter second number")
 a=int(a)
 b=int(b)
-c=(a+b)/2
-print ("The avg of two numbers is", c)
+d=(a+b)/2
+print ("The avg of two numbers is", d)
